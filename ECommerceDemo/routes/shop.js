5bf58e7a2bde0954badee75ac849f6790a218a8b
Ashish Pisey <ashishpisey21@gmail.com>
2023-08-18 19:22:00 +0530

    updated shop js router and shop style in html

--- ECommerceDemo/routes/shop.js
+++ ECommerceDemo/routes/shop.js
@@ -4,7 +4,14 @@ const rootDir = require('../utils/path')
 const router = express.Router()
 
 router.get('/', (_, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const productData = {
+        name: 'First Book',
+        price: '$24.99',
+        description: 'some book details'        
+    }
+    res.render('shop', {productData}) // this will execute shop.ejs
 })
+
 
 module.exports = router
